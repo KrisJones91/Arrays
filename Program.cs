b7ede633a982c65c79559ef32dea71f4f15d3654
KrisJones91 <kristop.jones@gmail.com>
2022-06-21 14:02:03 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] grades = new int[5];
 
-grades[0] = 20;
-grades[1] = 26;
-grades[2] = 54;
-grades[3] = 16;
-grades[4] = 3;
+Boolean valid = false;
+string inputValue;
 
-Console.WriteLine("grades at index 0: {0}", grades[0]);
-string input = Console.ReadLine();
+Console.Write("Enter a value: ");
+Console.WriteLine("Select the Data type to validate the input you have entered");
+Console.WriteLine("Press (1) for String");
+Console.WriteLine("Press (2) for Integer");
+Console.WriteLine("Press (3) for Boolean");
 
-grades[0] = int.Parse(input);
-Console.WriteLine("Grades at index 0: {0}", grades[0]);
-Console.ReadLine();
+Console.Write("Enter: ");
+int inputType = Convert.ToInt32(Console.ReadLine());
+
+static bool isAllAlhpabetic(string value)
+{
+
+}
 
 
-// another way of initializing an array
-int[] gradesOfMathStudentsA = { 20, 30, 40, 50, 60 };
 
-//third way of initializing an array
-int[] gradesOfMathStudentsB = new int[] { 15, 21, 35, 80, 90 };
-Console.WriteLine("Length of grades in class A: {0}", gradesOfMathStudentsA.Length);
-Console.ReadKey();
+
